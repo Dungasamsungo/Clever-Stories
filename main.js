e7f083208facb0a6b0e1b0d9e53eb60158537206
dungasamsungo <sampsonngobi@gmail.com>
2023-11-06 01:23:01 -0300

    Added  a button to generate the story

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-let adjective = document.getElementById('Adjective')
-let noun = document.getElementById('noun');
-let verb = document.getElementById('verb');
-let Exclamation = document.getElementById('Exclamation');
-let verb1 = document.getElementById('verb1');
-let verb2 = document.getElementById('verb2')
 
-var story = document.getElementById('story').innerHTML = `The other day, I was really in trouble. It all started when I saw a very
-${adjective} ${noun} + verb  + "down the hallway." + Exclamation +
-"I yelled. But all I could think to do was to" +
- verb1 + "over and over. Miraculously, that caused it to stop, but not before it tried to" +
-  verb2 + "right in front of my family.`
+document.getElementById("btn").addEventListener("click", generateStory)
+
+function generateStory() {
+    let adjective = document.getElementById('Adjective').value;
+    let noun = document.getElementById('noun').value;
+    let verb = document.getElementById('verb').value;
+    let Exclamation = document.getElementById('Exclamation').value;
+    let verb1 = document.getElementById('verb1').value;
+    let verb2 = document.getElementById('verb2').value;
+
+    let story = `The other day, I was really in trouble. It all started when I saw a very
+    ${adjective} ${noun} ${verb} "down the hallway." ${Exclamation}!
+    "I yelled. But all I could think to do was to" ${verb1} "over and over.
+    Miraculously, that caused it to stop, but not before it tried to"
+    ${verb2} "right in front of my family.`;
+
+    document.getElementById("story").innerHTML = story;
+
+}
+
+
+
+
  
  
 
