7d5692eff4d2e1e18c4799e84adc470067d58833
dungasamsungo <sampsonngobi@gmail.com>
2023-11-05 21:42:35 -0300

    replace HTML content with JS content

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let adjective = document.getElementById('Adjective');
+let adjective = document.getElementById('Adjective')
 let noun = document.getElementById('noun');
 let verb = document.getElementById('verb');
 let Exclamation = document.getElementById('Exclamation');
 let verb1 = document.getElementById('verb1');
 let verb2 = document.getElementById('verb2')
 
-document.getElementById('story').innerHTML =
- "The other day, I was really in trouble. It all started when I saw a very"
-  + adjective + noun + verb  + "down the hallway." + Exclamation +
-  "I yelled. But all I could think to do was to" +
-   verb1 + "over and over. Miraculously, that caused it to stop, but not before it tried to" +
-    verb2 + "right in front of my family."
-
+var story = document.getElementById('story').innerHTML = `The other day, I was really in trouble. It all started when I saw a very
+${adjective} ${noun} + verb  + "down the hallway." + Exclamation +
+"I yelled. But all I could think to do was to" +
+ verb1 + "over and over. Miraculously, that caused it to stop, but not before it tried to" +
+  verb2 + "right in front of my family.`
+ 
+ 
 
 
